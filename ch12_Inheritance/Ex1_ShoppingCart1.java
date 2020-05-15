9fca13c6311be4652e3154f1563a4f94dd80d68a
Caylem <caylemh@gmail.com>
2020-05-15 16:08:27 +0200

    Ch12_Ex3: Using the 'instanceof' Operator

--- ch12_Inheritance/Ex1_ShoppingCart1.java
+++ ch12_Inheritance/Ex1_ShoppingCart1.java
@@ -1,10 +1,20 @@
 public class Ex1_ShoppingCart1 {
         public static void main(String[] args){
     
-        // instantiate a Shirt object and call display() on the object reference
-        Shirt shirt1 = new Shirt(25.20, 'S', "Short Sleeve");
-        Shirt shirt2 = new Shirt(30.20, 'L', "Sleeveless");
-        shirt1.display(); 
-        shirt2.display();
+                // instantiate a Shirt object and call display() on the object reference
+                Ex1_Item1 item1 = new Shirt(25.20, 'S', "Short Sleeve", 'G');
+                Ex1_Item1 item2 = new Shirt(30.20, 'L', "Sleeveless", 'B');
+                
+                item1.display();
+                if (item1 instanceof Shirt) {
+                        String color = ((Shirt)item1).getColor();
+                        System.out.println("Color: "+color);     
+                } else System.out.println("Item is not a Shirt!");
+                
+                item2.display();
+                if (item2 instanceof Shirt) {
+                        String color = ((Shirt)item2).getColor();
+                        System.out.println("Color: "+color);     
+                } else System.out.println("Item is not a Shirt!");
         }
 }
--- ch12_Inheritance/Shirt.java
+++ ch12_Inheritance/Shirt.java
@@ -1,11 +1,13 @@
 public class Shirt extends Ex1_Item1 {
     private char pattern = 'C';
     private String cut = "-prove cut description-";
+    private char colorCode;
 
-    public Shirt(double price, char pattern, String cut) {
+    public Shirt(double price, char pattern, String cut, char colorCode) {
         super ("Shirt", price);
         this.pattern = pattern;
         this.cut = cut;
+        this.colorCode = colorCode;
     }
 
     public void display() {
@@ -15,5 +17,27 @@ public class Shirt extends Ex1_Item1 {
         super.display();
         System.out.println("\tPattern: "+pattern);
         System.out.println("\tCut: "+cut);
+        System.out.println("\tColor Code: "+colorCode);
+    }
+
+    public String getColor() {
+        String color = "";
+        switch (colorCode){
+            case 'R':
+                color = "Red";
+                break;
+            case 'G':
+                color = "Green";
+                break;
+            case 'B':
+                color = "Blue";
+                break;
+            case 'Y':
+                color = "Yellow";
+                break;
+            default:
+                color = "Invalid code";
+        }
+        return color;
     }
 }
